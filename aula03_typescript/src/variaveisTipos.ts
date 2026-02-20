f3a9f7fc27f82f621301a944f6c22f48ade83d7b
Peteru1303 <pedropaulo1303@gmail.com>
2026-02-20 20:31:56 -0300

    Aula 3, introdução de variaveis

---
+++ aula03_typescript/src/variaveisTipos.ts
@@ -0,0 +1,12 @@
+let nome: string = "joão"
+let idade: number = 25;
+let ativo: boolean = true;
+
+let dado: any;
+let vazio: null;
+let indefinido: undefined = undefined;
+
+let situacao: "ativo" | "inativo" | "meio_termo" = "ativo"
+let idadeOuNulo: string | null | 3 = 3;
+
+console.log(`Nome: ${nome}, Idade: ${idade}, Ativo: ${ativo}` );
