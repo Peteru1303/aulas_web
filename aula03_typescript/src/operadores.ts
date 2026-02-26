0fb9a411ea9f158e130b5826ffff3fe32c828cc9
Peteru1303 <pedropaulo1303@gmail.com>
2026-02-25 22:07:34 -0300

    comeco de operadores

---
+++ aula03_typescript/src/operadores.ts
@@ -0,0 +1,12 @@
+let a: number = 10;
+let b: number = 15;
+
+console.log("Soma: ", a + b);
+console.log("Subtração: ", a - b)
+console.log("Multiplicação: ", a * b)
+console.log("exponencialção: ", a ** b);
+console.log("igualdade: ", a == b);
+console.log("igualdade estrita: ", a === b);
+
+a++; // a = a + 1
+a += b; //a = a + b
